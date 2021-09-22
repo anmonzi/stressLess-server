3192308d5321170e1374ae437237ac4d3088d80d
Ashton Monzi <anmonzi@gmail.com>
2021-09-22 10:47:33 -0500

    added create method to motivation viewset

--- stresslessapi/views/motivation.py
+++ stresslessapi/views/motivation.py
@@ -1,5 +1,8 @@
 """View module for handling requests about admin motivations"""
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
@@ -26,3 +29,24 @@ class MotivationView(ViewSet):
 
         serializer = MotivationSerializer(motivations, many=False, context={'request': request})
         return Response(serializer.data)
+
+
+    def create(self, request):
+        """Handle POST operations for new motivations"""
+
+        # verify user and who is making post request
+        app_user = AppUser.objects.get(user=request.auth.user)
+
+        # create new instance of motivation
+        motivation = Motivation()
+        motivation.app_user = app_user
+        motivation.title = request.data["title"]
+        motivation.content = request.data["content"]
+        motivation.created_on = request.data["createdOn"]
+
+        try:
+            motivation.save()
+            serializer = MotivationSerializer(motivation, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
